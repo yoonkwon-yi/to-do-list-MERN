f7b90ee4a33291c9a07ded98d0ed0fcd0dddd859
yoonkwon-yi <yoonkwon.yi@gmail.com>
2021-11-01 10:01:55 -0500

    set up path to build folder

--- server.js
+++ server.js
@@ -4,6 +4,16 @@ const colors = require('colors') //logging with color
 const morgan = require('morgan') // HTTP request logger middleware for Node. js.
 const connectDB = require('./config/db')
 
+// Accessing the path module
+const path = require('path')
+
+// Step 1:
+app.use(express.static(path.resolve(__dirname, './client/build')))
+// Step 2:
+app.get('*', function (request, response) {
+  response.sendFile(path.resolve(__dirname, './client/build', 'index.html'))
+})
+
 dotenv.config({ path: './config/config.env' })
 
 connectDB()
